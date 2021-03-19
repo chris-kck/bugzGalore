598a041c728d1d0ddc2209a3896fce4cb26376de
Shingirai98 <https://github.com/Shingirai98>
2021-03-19 21:59:48 +0200

    one

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,25 @@ import React from 'react'
 function Header() {
   return (
     <div>
-      
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+          <Container>
+            <LinkContainer to='/'>
+              <Navbar.Brand>BugzShop</Navbar.Brand>
+            </LinkContainer>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+              <LinkContainer to='/'>
+                <Nav.Link><i className="fas fa-shopping-cart"></i>Cart</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/login'>
+                <Nav.Link><i className="fas fa-user"></i>Login</Nav.Link>
+              </LinkContainer>
+              </Nav>
+              
+            </Navbar.Collapse>
+            </Container>
+          </Navbar>
     </div>
   )
 }
