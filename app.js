e27197a15d77eef7de6517c5da4af653e368084d
Chris Kateera <chris.kateera@mohara.co>
2021-03-20 17:56:53 +0200

    Pay endpoint

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/', indexRouter);
+app.use('/pay', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 
 /* GET home page. */
-router.get('/pay', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 
     axios.post('https://online.yoco.com/v1/charges/', {
