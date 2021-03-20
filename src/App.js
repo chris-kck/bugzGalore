eced78d08fc1d841d7e46838efa13284e90f296d
Humphrey Chiramba <chirambaht@gmail.com>
2021-03-20 03:12:34 +0200

    Routes have been made planned

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import LogIn from "./LogIn"
-import SignUp from "./SignUp"
-import Landing from "./Landing"
-import Footer from "./Footer"
-
+import LogIn from "./LogIn";
+import SignUp from "./SignUp";
+import Landing from "./Landing";
+import Footer from "./Footer";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 function App() {
   return (
-    <div>
-    
-    <SignUp/>
-    <Footer/>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Landing />
+        </Route>
+        <Route path="/sign-up" exact>
+          <SignUp />
+        </Route>
+        <Route path="/sign-in" exact>
+          <LogIn />
+        </Route>
+        <Route path="/market/:id" exact>
+          {/* <Market /> */}
+        </Route>
+        <Route path="/vendor/:id" exact>
+          {/* <Vendor /> */}
+        </Route>
+
+        <Footer />
+      </Switch>
+    </Router>
   );
 }
 
