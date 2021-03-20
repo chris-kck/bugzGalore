143414b4c7cfb1f5874bc72828570d0d152db931
Humphrey Chiramba <chirambaht@gmail.com>
2021-03-20 16:56:57 +0200

    Updated StallCard error

--- src/components/StallCard.js
+++ src/components/StallCard.js
@@ -3,18 +3,19 @@ import React from "react";
 function StallCard({ image, stallName, description, email }) {
   return (
     <div>
-    <div class="grid justify-items-stretch">
-
-    <div class="justify-self-center">
-      <div class="max-w-xs rounded overflow-hidden shadow-lg my-2">
-        <img class="w-full" src={image} alt="Sunset in the mountains" />
-        <div class="px-6 py-4">
-          <div class="font-bold text-xl mb-2">{stallName}</div>
-          <p class="text-grey-darker text-base">{description}</p>
-          <p>
-            Please contact <strong>{stallName}</strong> on: <br />
-            <a href={`mailto:${email}`}>{email}</a>
-          </p>
+      <div class="grid justify-items-stretch">
+        <div class="justify-self-center">
+          <div class="max-w-xs rounded overflow-hidden shadow-lg my-2">
+            <img class="w-full" src={image} alt="Sunset in the mountains" />
+            <div class="px-6 py-4">
+              <div class="font-bold text-xl mb-2">{stallName}</div>
+              <p class="text-grey-darker text-base">{description}</p>
+              <p>
+                Please contact <strong>{stallName}</strong> on: <br />
+                <a href={`mailto:${email}`}>{email}</a>
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
