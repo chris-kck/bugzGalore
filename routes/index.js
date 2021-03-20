6cdb9d544daee37bce1267805569456b745c7042
Chris Kateera <chris.kateera@mohara.co>
2021-03-20 17:48:38 +0200

    Pay endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
+var axios = require('axios');
 var router = express.Router();
 
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/pay', function(req, res, next) {
   res.render('index', { title: 'Express' });
+
+    axios.post('https://online.yoco.com/v1/charges/', {
+      token: req.body.token,
+      currency: 'ZAR',
+      amountInCents: 5000
+    }, {
+      headers: {
+        'X-Auth-Secret-Key': 'sk_test_960bfde0VBrLlpK098e4ffeb53e1'
+      }
+    })
+        .then(response => {
+          res.send(response.data);
+        })
+        .catch(error => {
+          res.send(error.response.data);
+        })
 });
 
 module.exports = router;
