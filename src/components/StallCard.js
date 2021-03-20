8e24256dfff117576c4c37a68a2fff16f88ef352
Humphrey Chiramba <chirambaht@gmail.com>
2021-03-20 16:48:04 +0200

    Fixed up UI for customer

--- src/components/StallCard.js
+++ src/components/StallCard.js
@@ -1,24 +1,21 @@
-import React from 'react'
+import React from "react";
 
-function StallCard() {
+function StallCard({ image, stallName, description, email }) {
   return (
     <div>
       <div class="max-w-xs rounded overflow-hidden shadow-lg my-2">
-  <img class="w-full" src="https://images.unsplash.com/photo-1570169797118-99f5f90c4e25?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="Sunset in the mountains"/>
-  <div class="px-6 py-4">
-    <div class="font-bold text-xl mb-2">Book n Sip</div>
-    <p class="text-grey-darker text-base">
-      From the Tales to the Knowledge
-    </p>
-  </div>
-  <div class="px-6 py-4">
-    <span class="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker mr-2">#books</span>
-    <span class="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker mr-2">#stories</span>
-    <span class="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker">#leisure</span>
-  </div>
-</div>
+        <img class="w-full" src={image} alt="Sunset in the mountains" />
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2">{stallName}</div>
+          <p class="text-grey-darker text-base">{description}</p>
+          <p>
+            Please contact <strong>{stallName}</strong> on: <br />
+            <a href={`mailto:${email}`}>{email}</a>
+          </p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default StallCard
+export default StallCard;
--- src/components/StallView.js
+++ src/components/StallView.js
@@ -2,57 +2,30 @@ import React from "react";
 import logo from "./market.jpg";
 import Card from "./MarketCard";
 import StallCard from "./StallCard";
-import Footer from "./Footer";
+import { useParams } from "react-router";
+import dbData from "../assets/db";
 
 function StallView() {
-  const data = {
-    stalls: [
-      {
-        id: 0,
-        username: "Fun with Flags",
-        email: "sheldon@tbbt.com",
-        products: [
-          {
-            id: "prod-shel-1",
-            productName: "Zimbabwean Flag",
-          },
-          {
-            id: "prod-shel-2",
-            productName: "Kenyan Flag",
-          },
-          {
-            id: "prod-shel-3",
-            productName: "Sourh Africa(the country)",
-          },
-        ],
-      },
-      {
-        id: 1,
-        username: "Jess Day tutoring",
-        email: "jess@yahoo.com",
-        products: [
-          {
-            id: "prod-jess-1",
-            productName: "English Tutoring for kids",
-          },
-          {
-            id: "prod-jess-2",
-            productName: "Adult classes in English writing",
-          },
-        ],
-      },
-    ],
-  };
-
+  const { market, stall } = useParams();
+  const marketOwner = dbData.MarketOwners[market];
+  const stallInfo = marketOwner.stalls[stall];
   return (
     <div>
       <img src={logo} />
-      <Card />
-      <StallCard />
-      <button class="bg-gray-500  text-white font-bold py-2 px-4 border border-black-700 rounded">
-        Book Stall
-      </button>
-      <Footer />
+      <Card
+        marketName={marketOwner.marketName}
+        location={marketOwner.location}
+        distance={(Math.random() * 5).toFixed(2)}
+        image={marketOwner.image}
+        key={marketOwner.id}
+      />
+      <StallCard
+        stallName={stallInfo.stallName}
+        description={stallInfo.description}
+        email={stallInfo.email}
+        image={stallInfo.image}
+        key={stallInfo.id}
+      />
     </div>
   );
 }
